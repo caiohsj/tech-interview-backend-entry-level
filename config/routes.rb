0e6daeb60e9e40c9574f60226cfe142ca441a188
Caio Henrique <caiocoxim10@gmail.com>
2026-01-18 13:06:49 -0400

    feat: update routes to fix carts_routing_spec.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
-  resources :products
-  get "up" => "rails/health#show", as: :rails_health_check
 
-  root "rails/health#show"
+  resources :products
+
+  resources :carts, only: [:show, :create] do
+    post :add_item, on: :collection
+  end
+
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  root 'rails/health#show'
 end
